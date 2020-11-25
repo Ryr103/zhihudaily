c5c808508a516dee097ef53dedc7ef36a333a580
Ryr103 <1091752724@qq.com>
2020-11-25 22:07:19 +0800

    zhihudaily

--- app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -1,12 +1,31 @@
 package com.example.recyclerviewdemo;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
+
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,20 +33,233 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private Context context;
+    private List<Map<String,Object>> list = new ArrayList<>();
+    private TextView month;
+    private TextView day;
+    private RefreshLayout mReFreshLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        month = findViewById(R.id.tv_month);
+        day = findViewById(R.id.tv_day);
+
 
         recyclerView = findViewById(R.id.recyclerView);
-        List<Map<String, Object>> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("name", "小明" + i + "号");
-            list.add(map);
-        }
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));//垂直排列 , Ctrl+P
-        recyclerView.setAdapter(new MyAdapter(MainActivity.this, list));//绑定适配器
+        mReFreshLayout = findViewById(R.id.swipeReFreshLayout);
+        Calendar calendar = Calendar.getInstance();
+        int Month = calendar.get(Calendar.MONTH)+1;
+        month.setText(Month +"月");
+        int Day = calendar.get(Calendar.DAY_OF_MONTH);
+        day.setText(Day+"日");
+        final int[] date = {20200000+Month*100+Day};
+
+
+
+
+
+        mReFreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                list.clear();
+
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        HttpURLConnection connection = null;
+                        BufferedReader reader = null;
+                        try {
+                            URL url = new URL("https://news-at.zhihu.com/api/3/stories/latest");
+                            connection = (HttpURLConnection) url.openConnection();
+                            connection.setRequestMethod("GET");
+                            connection.setConnectTimeout(4000);
+                            connection.setReadTimeout(8000);
+                            InputStream in = connection.getInputStream();
+                            reader = new BufferedReader(new InputStreamReader(in));
+                            StringBuilder response = new StringBuilder();
+                            String line;
+                            while ((line = reader.readLine()) != null) {
+                                response.append(line);
+                            }
+                            showResponse(response.toString());
+                        } catch (Exception e) {
+
+                            e.printStackTrace();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this,"网络连接失败",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+
+                        } finally {
+                            if (reader != null) {
+                                try {
+                                    reader.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }
+                    }
+                });
+                thread.start();
+                mReFreshLayout.finishRefresh();
+            }
+        });
+        mReFreshLayout.setOnLoadmoreListener(new OnLoadmoreListener() {
+            @Override
+            public void onLoadmore(RefreshLayout refreshlayout) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        HttpURLConnection connection = null;
+                        BufferedReader reader = null;
+                        try {
+
+                                URL url = new URL("https://news-at.zhihu.com/api/3/stories/before/" + String.valueOf(date[0]));
+                                date[0] = date[0] -1;
+
+
+                            connection = (HttpURLConnection) url.openConnection();
+                            connection.setRequestMethod("GET");
+                            connection.setConnectTimeout(8000);
+                            connection.setReadTimeout(8000);
+                            InputStream in = connection.getInputStream();
+                            reader = new BufferedReader(new InputStreamReader(in));
+                            StringBuilder response = new StringBuilder();
+                            String line;
+                            while ((line = reader.readLine()) != null) {
+                                response.append(line);
+                            }
+                            showResponse(response.toString());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this,"网络连接失败",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } finally {
+                            if (reader != null) {
+                                try {
+                                    reader.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                });
+                
+                thread.start();
+                refreshlayout.finishLoadmore();
+            }
+        });
+
+
+
+
+
+
+
+
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL("https://news-at.zhihu.com/api/3/stories/latest");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this,"网络连接失败",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } finally {
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        });
+
+        thread.start();
+
+
+
+
     }
+    public void showResponse(final String string) {
+
+
+        try {
+            JSONObject jsonObject = new JSONObject(string);
+            String date = jsonObject.getString("date");
+            final JSONArray jsonArray = jsonObject.getJSONArray("stories");
+            for(int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                final String hint = jsonObject1.getString("hint");
+                final String title = jsonObject1.getString("title");
+                String imageUrl = jsonObject1.getJSONArray("images").getString(0);
+                String url = jsonObject1.getString("url");
+                String id =jsonObject1.getString("id");
+                Map<String, Object> map = new HashMap<>();
+                map.put("hint",hint);
+                map.put("title",title);
+                map.put("url",url);
+                map.put("id",id);
+                map.put("date",date);
+                map.put("imageUrl",imageUrl);
+                list.add(map);
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));//垂直排列 , Ctrl+P
+                    recyclerView.setAdapter(new MyAdapter(MainActivity.this, list));//绑定适配器
+                }
+            });
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/recyclerviewdemo/MyAdapter.java
+++ app/src/main/java/com/example/recyclerviewdemo/MyAdapter.java
@@ -1,46 +1,99 @@
 package com.example.recyclerviewdemo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
+
+public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    private static final int NORMAL_ITEM = 0;
+    private static final int GROUP_ITEM = 1;
     private List<Map<String, Object>> list;
     private Context context;
+
 
     public MyAdapter(Context context, List<Map<String, Object>> list) {
         this.context = context;
         this.list = list;
+
     }
+
+
 
     @NonNull
     @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+
+        View view1 = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+        View view2 = LayoutInflater.from(context).inflate(R.layout.item_title,parent,false);
+
+       if(viewType==NORMAL_ITEM) {
+           return new ViewHolder_1(view1);
+       }else if(viewType==GROUP_ITEM){
+           return new ViewHolder_2(view2);
+       }
+       return null;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, final int position) {
-        holder.textView.setText(list.get(position).get("name").toString());
 
-        holder.button.setOnClickListener(new View.OnClickListener() {
+
+    @Override
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, final int position) {
+        if (holder instanceof ViewHolder_1){
+        ((ViewHolder_1) holder).textView.setText(list.get(position).get("hint").toString());
+          ((ViewHolder_1) holder).articleTitle.setText(list.get(position).get("title").toString());
+            Glide.with(context).load(list.get(position).get("imageUrl").toString()).into(((ViewHolder_1) holder).imageView);
+          ((ViewHolder_1) holder).button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context,"你点击了第"+position+"个",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context,NewsActivity.class);
+                Object url = list.get(position).get("url");
+                Object id = list.get(position).get("id");
+                intent.putExtra("id",id.toString());
+                intent.putExtra("url",url.toString());
+                context.startActivity(intent);
             }
-        });
+        });}
+        else if(holder instanceof ViewHolder_2){
+            ((ViewHolder_2) holder).Date.setText(list.get(position).get("date").toString().substring(4,6)+"月"+list.get(position).get("date").toString().substring(6,8)+"日");
+            ((ViewHolder_2) holder).textView.setText(list.get(position).get("hint").toString());
+            ((ViewHolder_2) holder).articleTitle.setText(list.get(position).get("title").toString());
+            Glide.with(context)
+                    .load(list.get(position).get("imageUrl").toString())
+                    .into(((ViewHolder_2) holder).imageView);
+            ((ViewHolder_2) holder).button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(context,NewsActivity.class);
+                    Object url = list.get(position).get("url");
+                    Object id = list.get(position).get("id");
+                    intent.putExtra("id",id.toString());
+                    intent.putExtra("url",url.toString());
+                    context.startActivity(intent);
+
+
+                }
+            });
+        }
     }
 
     @Override
@@ -48,15 +101,52 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
         return list.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView textView;
-        private Button button;
-
-        ViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            textView = itemView.findViewById(R.id.text);
-            button = itemView.findViewById(R.id.btn);
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0 || list.get(position).get("date").toString()!=list.get(position-1).get("date").toString()) {
+            return GROUP_ITEM;
+        } else {
+            return NORMAL_ITEM;
         }
     }
+
+
+
+    public  class ViewHolder_1 extends RecyclerView.ViewHolder {
+        private TextView textView;
+        private TextView articleTitle;
+        private Button button;
+        private ImageView imageView;
+
+
+        ViewHolder_1(@NonNull View itemView) {
+            super(itemView);
+
+            textView = itemView.findViewById(R.id.Author);
+            button = itemView.findViewById(R.id.btn);
+            articleTitle = itemView.findViewById(R.id.articleTitle);
+            imageView = itemView.findViewById(R.id.picture);
+        }
+    }
+
+    public class ViewHolder_2 extends RecyclerView.ViewHolder {
+        private TextView textView;
+        private TextView articleTitle;
+        private Button button;
+        private TextView Date;
+        private ImageView imageView;
+
+
+        ViewHolder_2(@NonNull View itemView) {
+            super(itemView);
+
+            textView = itemView.findViewById(R.id.Author);
+            button = itemView.findViewById(R.id.btn);
+            articleTitle = itemView.findViewById(R.id.articleTitle);
+            Date = itemView.findViewById(R.id.date);
+            imageView = itemView.findViewById(R.id.picture);
+        }
+    }
+
+
 }
